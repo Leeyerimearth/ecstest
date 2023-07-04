1a16385b242cf344a64e575d6a0deb47e4b9a267
yerimlee <yerim.lee.earth@gmail.com>
2023-07-04 17:15:03 +0900

    ecs test

--- src/main/java/com/example/ecstest/HomeController.java
+++ src/main/java/com/example/ecstest/HomeController.java
@@ -1,7 +1,17 @@
 package com.example.ecstest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.ByteBuffer;
 
 @RestController
 public class HomeController {
@@ -15,5 +25,60 @@ public class HomeController {
     public String healthyCheck() {
 
         return "healthy!";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestBody MultipartFile imgFile) throws IOException, InterruptedException {
+
+        Toolkit toolKit = Toolkit.getDefaultToolkit();
+        MediaTracker mtracker = new MediaTracker(new Container());
+        Image image = toolKit.createImage(imgFile.getBytes());
+        mtracker.addImage(image, 1);
+        mtracker.waitForID(1);
+
+        System.out.println("이미지 가로 사이즈 : " + image.getWidth(null));
+        System.out.println("이미지 세로 사이즈 : " + image.getHeight(null));
+
+        String key = imgFile.getOriginalFilename();
+        imageFileUploadToS3("kiss-yerim-test", key, imgFile.getContentType(), imgFile.getBytes());
+
+        return "upload complete !";
+    }
+
+    @GetMapping("/getImage")
+    public String getImageUrl(@RequestParam String key) {
+        Region region = Region.AP_NORTHEAST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+        GetUrlRequest request = GetUrlRequest.builder()
+                .bucket("kiss-yerim-test")
+                .key(key)
+                .build();
+
+        URL url = s3.utilities().getUrl(request);
+
+        return url.toString();
+    }
+
+    public void imageFileUploadToS3(String bucket, String key, String contentType, byte[] fileData) {
+        Region region = Region.AP_NORTHEAST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        s3.deleteObject(deleteObjectRequest);
+        s3.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromByteBuffer(ByteBuffer.wrap(fileData)));
     }
 }
